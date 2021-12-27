4667a541ff809291e4c735327b3531a837243f88
walid-lam <66574026+walid-lam@users.noreply.github.com>
2021-12-27 09:49:47 +0100

    final version

---
+++ battleship/GameViewController.swift
@@ -0,0 +1,152 @@
+//
+//  GameViewController.swift
+//  battleship
+//
+//  Created by Walid Lamrini on 25/12/2021.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    var mSocket=SocketHandler.sharedInstance.getSocket();
+    @IBOutlet weak var bt1: UIButton!
+    @IBOutlet weak var bt2: UIButton!
+    @IBOutlet weak var bt3: UIButton!
+    @IBOutlet weak var bt4: UIButton!
+    @IBOutlet weak var bt5: UIButton!
+    @IBOutlet weak var bt6: UIButton!
+    @IBOutlet weak var bt7: UIButton!
+    @IBOutlet weak var bt8: UIButton!
+    @IBOutlet weak var bt9: UIButton!
+    @IBOutlet weak var labeltest: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SocketHandler.sharedInstance.establishedConnection();
+
+        
+    }
+    
+    
+    var grid = [Int](repeating: 0, count: 9)
+    
+    @IBAction func btn1(_ sender: Any) {
+        if(grid[0]==1){
+            bt1.setImage(UIImage(named: "blue.png"), for: .normal)
+            grid[0]=0
+        }
+        else{
+            bt1.setImage(UIImage(named: "ship.png"), for: .normal)
+            grid[0]=1;
+        }
+        
+    }
+    
+    @IBAction func btn2(_ sender: Any) {
+        if(grid[1]==1){
+            bt2.setImage(UIImage(named: "blue.png"), for: .normal)
+            grid[1]=0
+        }
+        else{
+            bt2.setImage(UIImage(named: "ship.png"), for: .normal)
+            grid[1]=1;
+        }
+    }
+    
+    @IBAction func btn3(_ sender: Any) {
+        if(grid[2]==1){
+            bt3.setImage(UIImage(named: "blue.png"), for: .normal)
+            grid[2]=0
+        }
+        else{
+            bt3.setImage(UIImage(named: "ship.png"), for: .normal)
+            grid[2]=1;
+        }
+    }
+    
+    @IBAction func btn4(_ sender: Any) {
+        if(grid[3]==1){
+            bt4.setImage(UIImage(named: "blue.png"), for: .normal)
+            grid[3]=0
+        }
+        else{
+            bt4.setImage(UIImage(named: "ship.png"), for: .normal)
+            grid[3]=1;
+        }
+    }
+    
+    @IBAction func btn5(_ sender: Any) {
+        if(grid[4]==1){
+            bt5.setImage(UIImage(named: "blue.png"), for: .normal)
+            grid[4]=0
+        }
+        else{
+            bt5.setImage(UIImage(named: "ship.png"), for: .normal)
+            grid[4]=1;
+        }
+    }
+    @IBAction func btn6(_ sender: Any) {
+        if(grid[5]==1){
+            bt6.setImage(UIImage(named: "blue.png"), for: .normal)
+            grid[5]=0
+        }
+        else{
+            bt6.setImage(UIImage(named: "ship.png"), for: .normal)
+            grid[5]=1;
+        }
+    }
+    
+    @IBAction func btn7(_ sender: Any) {
+        if(grid[6]==1){
+            bt7.setImage(UIImage(named: "blue.png"), for: .normal)
+            grid[6]=0
+        }
+        else{
+            bt7.setImage(UIImage(named: "ship.png"), for: .normal)
+            grid[6]=1;
+        }
+    }
+    
+    @IBAction func btn8(_ sender: Any) {
+        if(grid[7]==1){
+            bt8.setImage(UIImage(named: "blue.png"), for: .normal)
+            grid[7]=0
+        }
+        else{
+            bt8.setImage(UIImage(named: "ship.png"), for: .normal)
+            grid[7]=1;
+        }
+    }
+    @IBAction func btn9(_ sender: Any) {
+        if(grid[8]==1){
+            bt9.setImage(UIImage(named: "blue.png"), for: .normal)
+            grid[8]=0
+        }
+        else{
+            bt9.setImage(UIImage(named: "ship.png"), for: .normal)
+            grid[8]=1;
+        }
+    }
+    
+    @IBAction func goBtn(_ sender: Any) {
+        let string = grid.map {"\($0)"}.reduce("") { $0 + $1 }
+        mSocket.emit("confirmBoard", string)
+        
+        mSocket.on("gameCreated"){ ( dataArray, ack ) -> Void in
+            
+            let dataReceived = dataArray[0] as! String
+//            self.labeltest.text = dataReceived
+            if(dataReceived == "false"){
+                self.bt1.setImage(UIImage(named: "blue.png"), for: .normal)
+                self.bt2.setImage(UIImage(named: "blue.png"), for: .normal)
+                self.bt3.setImage(UIImage(named: "blue.png"), for: .normal)
+                self.bt4.setImage(UIImage(named: "blue.png"), for: .normal)
+                self.bt5.setImage(UIImage(named: "blue.png"), for: .normal)
+                self.bt6.setImage(UIImage(named: "blue.png"), for: .normal)
+                self.bt7.setImage(UIImage(named: "blue.png"), for: .normal)
+                self.bt8.setImage(UIImage(named: "blue.png"), for: .normal)
+                self.bt9.setImage(UIImage(named: "blue.png"), for: .normal);
+            }
+                }
+    }
+    
+}
--- battleship/ViewController.swift
+++ battleship/ViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     var mSocket=SocketHandler.sharedInstance.getSocket();
-  
-    @IBOutlet weak var label: UILabel!
+    
+    var user: String!
+    
+//    @IBOutlet weak var label: UILabel!
     
     @IBAction func btact(_ sender: Any) {
             mSocket.emit("counter")
         }
         
+    @IBOutlet weak var userlabel: UILabel!
     
     
     @IBOutlet weak var username: UITextField!
@@ -28,26 +31,36 @@ class ViewController: UIViewController {
     }
     @IBAction func signbtn(_ sender: Any) {
         mSocket.emit("login", ["username": username.text, "password": password.text])
+//        user = username.text
     }
     @IBAction func signupbtn(_ sender: Any) {
         mSocket.emit("register", ["username": username.text, "password": password.text])
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         SocketHandler.sharedInstance.establishedConnection();
                 
                 mSocket.on("status"){ ( dataArray, ack ) -> Void in
                     let dataReceived = dataArray[0] as! Int
                     if(dataReceived == 200){
                         self.performSegue(withIdentifier: "LoginSegue", sender: self)
+//                        if (self.restorationIdentifier != nil){
+//                            self.userlabel.text = self.user
+//                        }
                     }
                 }
+//        if (self.restorationIdentifier != nil){
+//            userlabel.text = user
+//        }
+        
+
+
         
 
         
                
     }
+
 
 //    @IBAction func ShowPopUp(_ sender: Any) {
 //        let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "sbPopUpID") as! PopUpViewController
